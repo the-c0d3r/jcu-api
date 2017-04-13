2023ee37235348ebe2ae54a314df80d74df20f3c
the-c0d3r <z3r0.mhu@gmail.com>
2017-04-13 09:45:17 +0800

    Changed to GET and POST files

---
+++ api-get.py
@@ -0,0 +1,65 @@
+from flask import Flask, jsonify
+from flask import request
+from lib.website import Website
+import json
+import time
+
+
+app = Flask(__name__)
+
+global website, lastUpdated
+lastUpdated = time.time()
+website = Website()
+
+
+@app.route("/classinfo")
+def classinfo():
+    updateDB()
+    subjcode = request.args.get('classname').upper()
+    result = website.getClassInfo(subjcode)
+    return jsonify(result.getDict()) if result != None else "No class information available"
+
+@app.route("/roominfo")
+def roominfo():
+    updateDB()
+    room = request.args.get('room').upper()
+    result = website.getRoomInfo(room)
+    # result is a list of classes
+    if len(result) > 0:
+        resultJson = jsonify([i.getDict() for i in result])
+        return resultJson
+    else:
+        return "No room information available"
+
+@app.route("/getclasses")
+def getclasses():
+    updateDB()
+    codes = request.args.get('codes').split(',')
+    if len(codes) == 0:
+        return "No subject codes given"
+    else:
+        result = website.getClasses(codes)
+        resultJson = jsonify([i.getDict() for i in result])
+        return resultJson
+
+# TODO handle incorrect API calls or parameter
+
+def updateDB():
+    global lastUpdated, website
+    now = time.time()
+    if now - lastUpdated >= 1800:
+        # meaning 30 minutes have passed
+        del website
+        website = Website()
+        # starts the parsing process again
+        lastUpdated = now
+        # update the last updated time
+
+@app.route("/getclasses")
+def getClasses():
+    updateDB()
+    codes = request.args.get("codes").split(",")
+    result = website.getClasses(codes)
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", debug=True)
